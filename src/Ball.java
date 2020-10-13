5354479bcecd6c1b392ab5767c5b65c1f843dfa0
sungmin.jung <jspiner@naver.com>
2020-10-13 13:42:40 +0900

    init

---
+++ src/Ball.java
@@ -0,0 +1,13 @@
+public class Ball {
+    int x = 100;
+    int y = 100;
+    int range = 10;
+    int length = 300;
+
+    public Ball(int x, int y, int range, int length) {
+        this.x = x;
+        this.y = y;
+        this.range = range;
+        this.length = length;
+    }
+}
