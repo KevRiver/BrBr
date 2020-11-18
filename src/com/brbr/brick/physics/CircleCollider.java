1bdec360d6de5f93a220f2b461eac0d3879f9c20
eunseo9808 <eunseo9808@naver.com>
2020-11-18 17:14:19 +0900

    feat: UIManager, TextUI, ButtonUI 구현

--- src/com/brbr/brick/physics/CircleCollider.java
+++ src/com/brbr/brick/physics/CircleCollider.java
@@ -38,47 +38,47 @@ public class CircleCollider extends Collider{
         Bounds bound = collider.bounds;
         if(bound.getMinX() <= center.x && center.x <= bound.getMaxX()){
             if(center.y <= bound.getMinY()) { // 사각형의 위에 위치
-                Debugger.Print("Relatively up");
+                //Debugger.Print("Relatively up");
                 return Vector2.up;
             }
             else { // 사각형의 아래에 위치
-                Debugger.Print("Relatively down");
+                //Debugger.Print("Relatively down");
                 return Vector2.down;
             }
         }
 
         if(bound.getMinY() <= center.y && center.y <= bound.getMaxY()){
             if(center.x <= bound.getMinX()) { // 사각형의 왼쪽에 위치
-                Debugger.Print("Relatively left");
+                //Debugger.Print("Relatively left");
                 return Vector2.left;
             }
             else { // 사각형의 오른쪽에 위치
-                Debugger.Print("Relatively right");
+                //Debugger.Print("Relatively right");
                 return Vector2.right;
             }
         }
 
         if(center.x <= bound.getMinX() && center.y <= bound.getMinY()){ // 사각형의 왼쪽 상단에 위치
-            Debugger.Print("Relatively leftUp");
+            //Debugger.Print("Relatively leftUp");
             return Vector2.leftUp;
         }
 
         if(center.x >= bound.getMaxX() && center.y <= bound.getMinY()){ // 사각형의 오른쪽 상단에 위치
-            Debugger.Print("Relatively rightUp");
+            //Debugger.Print("Relatively rightUp");
             return Vector2.rightUp;
         }
 
         if(center.x <= bound.getMinX() && center.y >= bound.getMaxY()){ // 사각형의 왼쪽 하단에 위치
-            Debugger.Print("Relatively leftDown");
+            //Debugger.Print("Relatively leftDown");
             return Vector2.leftDown;
         }
 
         if(center.x >= bound.getMaxX() && center.y >= bound.getMaxY()){ // 사각형의 오른쪽 하단에 위치
-            Debugger.Print("Relatively righttDown");
+            //Debugger.Print("Relatively righttDown");
             return Vector2.rightDown;
         }
 
-        Debugger.Print("Relatively up");
+        //Debugger.Print("Relatively up");
         return Vector2.up;
 
     }
