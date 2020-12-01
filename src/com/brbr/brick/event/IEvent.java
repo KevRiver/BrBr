0818ee63e17bb67536b1d7c41da2056a6459a515
Seung Gon Kang <kanggakky1214@gmail.com>
2020-12-01 15:25:45 +0900

    Update IEvent.java

--- src/com/brbr/brick/event/IEvent.java
+++ src/com/brbr/brick/event/IEvent.java
@@ -3,7 +3,7 @@ package com.brbr.brick.event;
 import java.util.EventObject;
 
 /// T : IEventListener, U another event
-public interface IEvent<T extends IEventListener, U extends EventObject> {
+public interface IEvent<T extends IEventListener, U> {
     void raise(U u);
     void addListener(T t);
     void removeListener(T t);
