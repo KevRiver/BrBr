7582be122ded54c26755decd0ebfb131c814b7d4
정성민 <jspiner@naver.com>
2020-12-03 18:02:54 +0900

    Update src/com/brbr/brick/object/Brick.java
    Co-authored-by: 7kQon <kanggakky1214@gmail.com>

--- src/com/brbr/brick/object/Brick.java
+++ src/com/brbr/brick/object/Brick.java
@@ -34,7 +34,6 @@ public class Brick extends AnimationObject {
 
     @Override
     public void onCollisionEnter(Collider collider) {
-        super.onCollisionEnter(collider);
         this.health--;
     }
 }
